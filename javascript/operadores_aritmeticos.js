0e3f74576bf0fd2fff6d9873b5e7b7a17fb95f53
Denilson <denilsongunner@gmail.com>
2023-05-25 07:52:32 -0300

    Operadores Aritméticos

---
+++ javascript/operadores_aritmeticos.js
@@ -0,0 +1,30 @@
+//Operadores Aritméticos
+
+let numero1 = 20;
+let numero2 = 2;
+
+//Adição            +
+console.log('Soma =', numero1 + numero2)
+
+//Subtração         -
+console.log('Subtração =', numero1 - numero2)
+
+//Multiplicação     *
+console.log('Multiplicação =', numero1 * numero2)
+
+//Divisão           /
+console.log('Divisão =', numero1 / numero2)
+
+//Exponenciação     **
+console.log('Exponenciação =', numero1 ** numero2)
+
+//Resto de divisão  %
+console.log('Módulo =', numero1 % numero2)
+
+//Precedência de operadores
+console.log((3+7)*3)
+console.log((10+7+8)/3)
+
+//Biblioteca Math
+console.log(Math.PI)
+console.log(Math.sqrt(16))
