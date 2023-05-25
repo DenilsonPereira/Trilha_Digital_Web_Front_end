e9c9a7cac799886d4d44eb70dc88f1ec6f6c67e6
Denilson <denilsongunner@gmail.com>
2023-05-25 12:38:31 -0300

    Estruturas Condicionais

---
+++ javascript/estruturas_condicionais.js
@@ -0,0 +1,25 @@
+//Estruturas Condicionais
+
+const idade = 25;
+
+if (idade >= 18){
+    console.log('Maior de idade');
+}else{
+    console.log('Menor de idade');
+}
+
+// Se media>= 7, Aprovado
+// Se media>=5, Recuperação
+// Se media<5, Reprovado
+
+console.clear(); //Limpando as saídas anteriores
+
+media = 4;
+
+if (media>= 7){
+    console.log('Aprovado');
+}else if(media>=5){
+    console.log('Recuperação');
+}else{
+    console.log('Reprovado');
+}
