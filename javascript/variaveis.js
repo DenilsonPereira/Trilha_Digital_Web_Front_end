d9cae5e3f16a761473769a8859b4fd332ef354f5
Denilson <denilsongunner@gmail.com>
2023-05-25 07:40:00 -0300

    Variáveis | Operadores

---
+++ javascript/variaveis.js
@@ -0,0 +1,26 @@
+//VARIÁVEIS
+
+let idade = 26;
+console.log(idade)
+
+//Tipos de variáveis
+
+
+idade=30;                 //number
+let numero = 30;         //number
+let altura = 1.77;      //number
+let nome = 'João';     //string
+let estudando = true; //boolean
+
+console.log(numero, typeof numero)
+console.log(altura)
+console.log(nome)
+console.log(estudando)
+
+let outra;
+console.log(outra)
+
+let nula = null;
+console.log(nula)
+
+const constante = 100;
