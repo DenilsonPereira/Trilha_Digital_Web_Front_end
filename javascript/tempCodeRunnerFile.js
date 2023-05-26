a296062140ddc1d7cc3068bbb9193e49e361ec51
Denilson <denilsongunner@gmail.com>
2023-05-26 09:10:34 -0300

    Update tempCodeRunnerFile.js

--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,1 +1,20 @@
-console.log('Tamanho do array: ',arr.length)
+// Arrays (parte II) - Métodos de array
+
+let arr1 = [30, 12, 25, 34, 82];
+let arr2 = [];
+
+// Fatiamento - Slice
+
+console.log(arr1.slice(0, 2));
+console.log(arr1.slice(2));
+console.log(arr1.slice(1 | 2));
+
+console.clear();
+
+// Adicionando elementos: push | unshift
+
+console.log(arr2);
+
+arr2.push(5,2);
+arr2.unshift(15,3);
+console.log(arr2);
